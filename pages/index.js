50b1df0f04f659f93d72adff16a6335c87f5d889
sourav suman <saurav2fly@gmail.com>
2020-04-04 14:34:35 +0530

    we are done with header and button

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react' ;
+import { Card , Button} from 'semantic-ui-react' ;
 import factory from '../ethereum/factory' ;
+import Layout from '../components/Layout' ;
 
 class CampaignIndex extends Component {
-  async componentDidMount() {
-    const campaigns = await factory.methods.getDeployedCampaigns().call();
-    console.log(campaigns);
-  }
+
+static async getInitialProps() {
+  const campaigns = await factory.methods.getDeployedCampaigns().call();
+    return{campaigns};
+}
+
+renderCampaigns() {
+  const items =this.props.campaigns.map(address =>{
+    return {
+      header :address ,
+      description :<a>View Campaigns</a>,
+      fluid : true
+    };
+  });
+  return<Card.Group items = {items} />
+}
   render() {
-    return <div>Campaigns index !!</div>
+    return(
+    <Layout>
+    <div>
+   <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" />
+   <h3>Open Campaigns</h3>
+
+   <Button
+   floated ="right"
+   content ="Create Campaign"
+   icon ="add circle"
+   primary
+   />
+    {this.renderCampaigns()}
+   </div>
+    </Layout>
+  );
   }
 }
 
