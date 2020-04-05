21f3598c69175521c62148f1d84ecb3ebd0a4b37
sourav suman <saurav2fly@gmail.com>
2020-04-05 17:11:36 +0530

    done with ascessing Campaigns on console

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  '0x4724B92DB40c586984C22e74420800c3782a3FB6'
+  '0xB61F62e1269c9aB92dB15EBee91B165BF9F401ce'
 );
 
 export default instance ;
