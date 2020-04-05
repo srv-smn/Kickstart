49fe4d57ad86f6814fe3bb260dd22a753259080b
sourav suman <saurav2fly@gmail.com>
2020-04-05 07:03:32 +0530

    done with create new campaigns page

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,6 @@ renderCampaigns() {
     return(
     <Layout>
     <div>
-   <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" />
    <h3>Open Campaigns</h3>
 
    <Button
